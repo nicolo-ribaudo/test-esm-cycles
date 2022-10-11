e970dc530080a85d82033a7d399e36a9ff6d16bb
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-10-11 18:40:25 +0200

    Initial commit

---
+++ a.js
@@ -0,0 +1,3 @@
+export { b } from "./b.js";
+export { c } from "./c.js";
+export { d } from "./d.js";
---
+++ c.js
@@ -0,0 +1,4 @@
+import { b } from "./b.js";
+import { d } from "./d.js";
+
+export const c = b + 100 + d;
